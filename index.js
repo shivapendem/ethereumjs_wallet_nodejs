fe53fad63941d15fd6569c2935f8b4105dec68b4
shivapendem <pendem.shiva89@gmail.com>
2021-05-26 16:59:15 +0530

    updated with version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var Wallet = require('ethereumjs-wallet');
-const EthWallet = Wallet.generate();
+const EthWallet = Wallet.default.generate();
 console.log("address: " + EthWallet.getAddressString());
 console.log("privateKey: " + EthWallet.getPrivateKeyString());
